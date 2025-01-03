6ca2d386a1bf877c592217203383d87cd5a15bcb
ARYAN TYAGI <aryantyagi111s@gmail.com>
2025-01-03 13:17:43 +0530

    fixing origin issue

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,10 +1,17 @@
+// export const corsOption = {
+//   origin: [
+//     "http://localhost:5173",
+//     "https://localhost:4173",
+//     "https://gaming-app-client.vercel.app/",
+//     process.env.CLIENT_URL,
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// };
+
+
 export const corsOption = {
-  origin: [
-    "http://localhost:5173",
-    "https://localhost:4173",
-    "https://gaming-app-client.vercel.app/",
-    process.env.CLIENT_URL,
-  ],
+  origin: true,  // Allows all origins
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
