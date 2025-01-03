69ca356a0c601bc2dd9c4f407ce132f719aeda15
ARYAN TYAGI <aryantyagi111s@gmail.com>
2025-01-03 13:13:53 +0530

    fixing server

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,6 +2,7 @@ export const corsOption = {
   origin: [
     "http://localhost:5173",
     "https://localhost:4173",
+    "https://gaming-app-client.vercel.app/",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
