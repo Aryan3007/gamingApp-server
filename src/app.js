a958f3079a7f24d2d2b9ffdefe8f519e85246562
Dhruv Gupta <kirangupta1218@gmail.com>
2025-01-12 16:55:05 +0530

    api integrate for all sports

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { corsOption } from "./constants/config.js";
-import { errorMiddleware } from "./middlewares/error.js";
+import { errorMiddleware, TryCatch } from "./middlewares/error.js";
 import { connectDB } from "./utils/features.js";
-import { fetchDataPeriodically, latestData } from "./utils/service.js";
 
 import userRoute from "./routes/user.js";
 import paymentRoute from "./routes/payment.js";
+import axios from "axios";
 
 config({
   path: "./.env",
@@ -19,15 +19,26 @@ config({
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || "";
 const NODE_ENV = process.env.NODE_ENV.trim() || "PRODUCTION";
-const API_BASE_URL = process.env.API_BASE_URL || "https://api.aiodds.com/v1";
-const API_USER = process.env.API_USER || "";
-const API_SECRET = process.env.API_SECRET || "";
+const API_BASE_URL = process.env.API_BASE_URL || "";
+const API_TOKEN = process.env.API_TOKEN || "";
 
 connectDB(MONGO_URI);
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+export const io = new Server(server, {
+  cors: {
+    // origin: [
+    //   "http://localhost:5173",
+    //   "http://localhost:3000",
+    //   "http://localhost:4173",
+    //   "*",
+    // ],
+    origin: true,
+    credentials: true,
+  },
+  path: "/socket.io/",
+});
 
 app.use(express.json());
 app.use(cookieParser());
@@ -35,26 +46,54 @@ app.use(cors(corsOption));
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/payment", paymentRoute);
-app.use("/api/v1/latest-data", latestData);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+let sportsData = {};
+
+const fetchSportsData = TryCatch(async (req, res, next) => {
+  const sportIds = [1, 3, 13, 18, 2, 4, 9];
+  const promises = sportIds.map((id) =>
+    axios.get(`${API_BASE_URL}/events/inplay?sport_id=${id}&token=${API_TOKEN}`)
+  );
+
+  const responses = await Promise.allSettled(promises);
+
+  sportsData = {};
+  responses.forEach((result, index) => {
+    if (result.status === "fulfilled") {
+      sportsData[sportIds[index]] = result.value.data.results;
+    } else {
+      console.error(
+        `Error fetching data for sport ID ${sportIds[index]}:`,
+        result.reason.message
+      );
+      sportsData[sportIds[index]] = null;
+    }
+  });
+
+  io.emit("sportsData", sportsData);
+  // console.log("Updated sports data:", sportsData);
+});
+
+setInterval(fetchSportsData, 5000);
+
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
+
+  socket.emit("sportsData", sportsData);
 
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
   });
 });
 
-// fetchDataPeriodically(io);
-
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is working on port ${PORT} in ${NODE_ENV} mode`);
 });
 
-export { NODE_ENV, API_BASE_URL, API_SECRET, API_USER };
+export { NODE_ENV };
--- src/utils/service.js
+++ src/utils/service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_BASE_URL, API_SECRET, API_USER } from "../../src/app.js";
+import { API_BASE_URL, API_TOKEN } from "../../src/app.js";
 import { Odds } from "../models/historicalOdds.js";
 import { TryCatch } from "../middlewares/error.js";
 
@@ -8,151 +8,148 @@ let scores = {};
 let odds = {};
 const marketIds = [10, 32, 83];
 const apiCallTime = 10; // in seconds
+const sportIds = [1, 3, 13, 18, 2, 4, 9];
 
-const fetchLiveMatches = async () => {
-  // https://api.aiodds.com/v1/match/list?user=khhbdsu&secret=692ceaccd6ea4e0fb504044ad5360178&sport_id=5&status_id=3
-  try {
-    const response = await axios.get(
-      `${API_BASE_URL}/match/list?user=${API_USER}&secret=${API_SECRET}&sport_id=5&status_id=3`
-    );
-    liveMatches = response.data.data.list;
-    // console.log(liveMatches);
-  } catch (error) {
-    console.error("Error fetching live matches:", error.message);
-  }
-};
-
-const fetchScores = async () => {
-  // https://api.aiodds.com/v1/match/score/get?user=khhbdsu&secret=692ceaccd6ea4e0fb504044ad5360178&id=13277589
-  try {
-    for (const match of liveMatches) {
-      const response = await axios.get(
-        `${API_BASE_URL}/match/score/get?user=${API_USER}&secret=${API_SECRET}&id=${match.id}`
-      );
-      scores[match.id] = response.data.data;
-    }
-    // console.log("Scores:", scores);
-  } catch (error) {
-    console.error("Error fetching scores:", error.message);
-  }
-};
-
-const fetchOdds = async () => {
-  // https://api.aiodds.com/v1/odds/inplay/market/bookmakers?user=khhbdsu&secret=692ceaccd6ea4e0fb504044ad5360178&sport_id=5&match_id=13277589&market_id=10
-  try {
-    for (const match of liveMatches) {
-      odds[match.id] = {};
-
-      const marketPromises = marketIds.map(async (marketId) => {
-        const response = await axios.get(
-          `${API_BASE_URL}/odds/inplay/market/bookmakers?user=${API_USER}&secret=${API_SECRET}&sport_id=5&match_id=${match.id}&market_id=${marketId}`
-        );
-        odds[match.id][marketId] = response.data.data;
-
-        await Odds.create({
-          matchId: match.id,
-          marketId,
-          oddsData: response.data.data,
-        });
-      });
-
-      await Promise.all(marketPromises);
-    }
-    // console.log("odds:", odds);
-  } catch (error) {
-    console.error("Error fetching odds:", error.message);
-  }
-};
-
-const getLastTwoHistoricalOdds = async (matchId, marketId) => {
-  try {
-    const historicalOdds = await Odds.find({ matchId, marketId })
-      .sort({ createdAt: -1 })
-      .limit(2);
-
-    return historicalOdds.reverse();
-  } catch (error) {
-    console.error(`Error historical odds for: ${matchId}:`, error.message);
-    return [];
-  }
-};
-
-const latestData = TryCatch(async (req, res, next) => {
-  await fetchLiveMatches();
-  await fetchScores();
-  await fetchOdds();
-
-  const combinedData = await Promise.all(
-    liveMatches.map(async (match) => {
-      const historicalOdds = {};
-
-      for (const marketId of marketIds) {
-        historicalOdds[marketId] = await getLastTwoHistoricalOdds(
-          match.id,
-          marketId
-        );
-      }
-
-      return {
-        ...match,
-        score: scores[match.id] || null,
-        odds: odds[match.id] || null,
-        historicalOdds,
-      };
-    })
+const fetchAllSports = TryCatch(async (req, res, next) => {
+  // https://api.b365api.com/v3/events/inplay?sport_id=3&token=211550-FilIMrGz4rRPZY
+  const response = await axios.get(
+    `${API_BASE_URL}/events/inplay?sport_id=3&token=${API_TOKEN}`
   );
-
-  return res.status(200).json({
-    message: "Data fetched successfully",
-    combinedData,
-  });
+  console.log(response.data);
 });
+fetchAllSports();
 
-const fetchDataPeriodically = (io) => {
-  setInterval(async () => {
-    console.log("Fetching data...");
-    try {
-      await fetchLiveMatches();
-      // console.log("Live matches fetched:", liveMatches);
+// const fetchScores = async () => {
+//   // https://api.aiodds.com/v1/match/score/get?user=khhbdsu&secret=692ceaccd6ea4e0fb504044ad5360178&id=13277589
+//   try {
+//     for (const match of liveMatches) {
+//       const response = await axios.get(
+//         `${API_BASE_URL}/match/score/get?user=${API_USER}&secret=${API_SECRET}&id=${match.id}`
+//       );
+//       scores[match.id] = response.data.data;
+//     }
+//     // console.log("Scores:", scores);
+//   } catch (error) {
+//     console.error("Error fetching scores:", error.message);
+//   }
+// };
 
-      await fetchScores();
-      // console.log("Scores fetched:", scores);
+// const fetchOdds = async () => {
+//   // https://api.aiodds.com/v1/odds/inplay/market/bookmakers?user=khhbdsu&secret=692ceaccd6ea4e0fb504044ad5360178&sport_id=5&match_id=13277589&market_id=10
+//   try {
+//     for (const match of liveMatches) {
+//       odds[match.id] = {};
 
-      await fetchOdds();
-      // console.log("Odds fetched:", odds);
+//       const marketPromises = marketIds.map(async (marketId) => {
+//         const response = await axios.get(
+//           `${API_BASE_URL}/odds/inplay/market/bookmakers?user=${API_USER}&secret=${API_SECRET}&sport_id=5&match_id=${match.id}&market_id=${marketId}`
+//         );
+//         odds[match.id][marketId] = response.data.data;
 
-      const combinedData = await Promise.all(
-        liveMatches.map(async (match) => {
-          const historicalOdds = {};
+//         await Odds.create({
+//           matchId: match.id,
+//           marketId,
+//           oddsData: response.data.data,
+//         });
+//       });
 
-          for (const marketId of marketIds) {
-            const lastTwoOdds = await getLastTwoHistoricalOdds(
-              match.id,
-              marketId
-            );
-            // console.log(
-            //   `Historical odds for match ${match.id}, market ${marketId}:`,
-            //   lastTwoOdds
-            // );
+//       await Promise.all(marketPromises);
+//     }
+//     // console.log("odds:", odds);
+//   } catch (error) {
+//     console.error("Error fetching odds:", error.message);
+//   }
+// };
 
-            historicalOdds[marketId] = lastTwoOdds;
-          }
+// const getLastTwoHistoricalOdds = async (matchId, marketId) => {
+//   try {
+//     const historicalOdds = await Odds.find({ matchId, marketId })
+//       .sort({ createdAt: -1 })
+//       .limit(2);
 
-          return {
-            ...match,
-            score: scores[match.id] || null,
-            odds: odds[match.id] || null,
-            historicalOdds,
-          };
-        })
-      );
+//     return historicalOdds.reverse();
+//   } catch (error) {
+//     console.error(`Error historical odds for: ${matchId}:`, error.message);
+//     return [];
+//   }
+// };
 
-      io.emit("updateData", combinedData);
-      console.log("Combined data emitted to WebSocket:", combinedData);
-    } catch (error) {
-      console.error("Error fetching data periodically:", error.message);
-    }
-  }, apiCallTime * 1000);
-};
+// const latestData = TryCatch(async (req, res, next) => {
+//   await fetchLiveMatches();
+//   await fetchScores();
+//   await fetchOdds();
+
+//   const combinedData = await Promise.all(
+//     liveMatches.map(async (match) => {
+//       const historicalOdds = {};
+
+//       for (const marketId of marketIds) {
+//         historicalOdds[marketId] = await getLastTwoHistoricalOdds(
+//           match.id,
+//           marketId
+//         );
+//       }
+
+//       return {
+//         ...match,
+//         score: scores[match.id] || null,
+//         odds: odds[match.id] || null,
+//         historicalOdds,
+//       };
+//     })
+//   );
+
+//   return res.status(200).json({
+//     message: "Data fetched successfully",
+//     combinedData,
+//   });
+// });
+
+// const fetchDataPeriodically = (io) => {
+//   setInterval(async () => {
+//     console.log("Fetching data...");
+//     try {
+//       await fetchLiveMatches();
+//       // console.log("Live matches fetched:", liveMatches);
+
+//       await fetchScores();
+//       // console.log("Scores fetched:", scores);
+
+//       await fetchOdds();
+//       // console.log("Odds fetched:", odds);
+
+//       const combinedData = await Promise.all(
+//         liveMatches.map(async (match) => {
+//           const historicalOdds = {};
+
+//           for (const marketId of marketIds) {
+//             const lastTwoOdds = await getLastTwoHistoricalOdds(
+//               match.id,
+//               marketId
+//             );
+//             // console.log(
+//             //   `Historical odds for match ${match.id}, market ${marketId}:`,
+//             //   lastTwoOdds
+//             // );
+
+//             historicalOdds[marketId] = lastTwoOdds;
+//           }
+
+//           return {
+//             ...match,
+//             score: scores[match.id] || null,
+//             odds: odds[match.id] || null,
+//             historicalOdds,
+//           };
+//         })
+//       );
+
+//       io.emit("updateData", combinedData);
+//       console.log("Combined data emitted to WebSocket:", combinedData);
+//     } catch (error) {
+//       console.error("Error fetching data periodically:", error.message);
+//     }
+//   }, apiCallTime * 1000);
+// };
 
 export { fetchDataPeriodically, latestData };
