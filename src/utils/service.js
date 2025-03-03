50f6c88b533d81fb14aa336bad2e7ac43d7951e7
ARYAN TYAGI <aryantyagi111s@gmail.com>
2025-03-03 22:47:28 +0530

    reducing cache time

--- src/utils/service.js
+++ src/utils/service.js
@@ -4,7 +4,7 @@ import { API_BASE_URL } from "../../src/app.js";
 import { TryCatch } from "../middlewares/error.js";
 import { ErrorHandler } from "./utility-class.js";
 
-const cache = new NodeCache({ stdTTL: 600 }); // Cache TTL set to 10 minutes
+const cache = new NodeCache({ stdTTL: 3 }); // Cache TTL set to 3 sec
 
 const chunkArray = (array, size) => {
   const chunks = [];
